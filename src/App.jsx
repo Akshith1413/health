cfdaf9a75e0f404fbf8bd13c8b8b49ccce902953
Akshith <ravulaakshith1@gmail.com>
2025-08-06 02:06:23 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from '/src/context/AuthContext.jsx';
 import AuthGate from './components/AuthGate';
 import AppRoutes from './routes/AppRoutes';
